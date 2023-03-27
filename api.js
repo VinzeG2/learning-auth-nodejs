06d65a1b0cece75ee3a5210b9d1794c20c1759c8
vinzeG2 <vinze.g2@gmail.com>
2023-03-26 20:03:25 -0400

    auth controller

--- api.js
+++ api.js
@@ -2,15 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const spell = require('./spells.controller')
 const mongoose = require('mongoose')
+const { Auth, isAuthenticated } = require('./auth.controller')
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.klxnqmt.mongodb.net/spellsproject?retryWrites=true&w=majority`)
 const app = express();
 app.use(express.json())
 
-app.get('/spells', spell.listAll)
-app.get('/spells/:id', spell.get)
-app.post('/spells', spell.create)
-app.delete('/spells/:id', spell.delete)
+app.get('/spells', isAuthenticated, spell.listAll)
+app.get('/spells/:id', isAuthenticated, spell.get)
+app.post('/spells', isAuthenticated, spell.create)
+app.delete('/spells/:id', isAuthenticated, spell.delete)
+
+app.post('/login', Auth.login)
+app.post('/register', Auth.register)
 
 app.use(express.static('app'))
 
--- auth.controller.js
+++ auth.controller.js
@@ -1,7 +1,27 @@
 const UserModel = require("./User")
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
+const express = require('express')
 
+const validateJwt = expressJwt.expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })
+
+const findAndAssignUser = async (req, res, next) => {
+    try {
+        const user = await UserModel.findById(req.auth._id)
+        if (!user) {
+            return res.status(401).end()
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJwt,findAndAssignUser)
+
+const signToken = (_id) => jwt.sign({ _id }, process.env.SECRET)
 
 const Auth = {
     login: async (req, res) => {
@@ -13,12 +33,33 @@ const Auth = {
             } else {
                 const isMatch = await bcrypt.compare(body.password, user.password)
                 if (isMatch) {
-                    const signed = jwt.sign({ _id }, process.env.SECRET)
+                    const signed = signToken(user._id)
+                    res.status(200).send(signed)
+                } else {
+                    return res.status(401).send('Wrong username or password.')
                 }
             }
         } catch (error) {
-            
+            res.send(error.message)
         }
     },
-    register: () => {},
+    register: async (req, res) => {
+        const { body } = req
+        try {
+            const alreadyExist = await UserModel.findOne({username: body.username})
+            if (alreadyExist) {
+                return res.send("User already exist.")
+            } else {
+                const salt = await bcrypt.genSalt()
+                const hashed = await bcrypt.hash(body.password, salt)
+                const user = await UserModel.create({username: body.username, password: hashed, salt: salt})
+                const signed = signToken(user._id)
+                res.send(signed)
+            }
+        } catch (error) {
+            res.status(500).send(error.message)
+        }
+    },
 }
+
+module.exports = { Auth, isAuthenticated }
